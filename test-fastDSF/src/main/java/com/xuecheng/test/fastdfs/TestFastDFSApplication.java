e4b18eb5350194c6e5ffbde75ea031163514d0e3
guoyicong <836732982@qq.com>
2019-01-24 22:22:59 +0800

    课程查看添加都完成了

---
+++ test-fastDSF/src/main/java/com/xuecheng/test/fastdfs/TestFastDFSApplication.java
@@ -0,0 +1,19 @@
+package com.xuecheng.test.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * project name : xuecheng
+ * Date:2019/1/22
+ * Author: yc.guo
+ * DESC:
+ */
+@SpringBootApplication
+public class TestFastDFSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestFastDFSApplication.class,args);
+
+    }
+    
+}
